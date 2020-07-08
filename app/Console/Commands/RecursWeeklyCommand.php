f5f9a95d47f8034d053135a5270066d40ac1fa5f
peekinternational <peekinternational@gmail.com>
2020-07-08 15:01:33 +0500

    recurs_weeklyss

--- app/Console/Commands/RecursWeeklyCommand.php
+++ app/Console/Commands/RecursWeeklyCommand.php
@@ -40,19 +40,40 @@ class RecursWeeklyCommand extends Command
     public function handle()
     {
     // Check cron job by sending email
-    $get_user = DB::table('users')->where('id',2)->first();
-      $toemail=$get_user->email;
-      $name = 'waqas';
-      $agreement_id = 1;
-      Mail::send('mail.check_cronjob_email',['user' =>$get_user,'agreement_id'=>$agreement_id],
-      function ($message) use ($toemail)
-      {
+    // $get_user = DB::table('users')->where('id',2)->first();
+    //   $toemail=$get_user->email;
+    //   $name = 'waqas';
+    //   $agreement_id = 1;
+    //   Mail::send('mail.check_cronjob_email',['user' =>$get_user,'agreement_id'=>$agreement_id],
+    //   function ($message) use ($toemail)
+    //   {
+    //
+    //     $message->subject('Smart Cookie Tutors.com - Cron Job Email');
+    //     $message->from('admin@SmartCookieTutors.com', 'Smart Cookie Tutors');
+    //     $message->to($toemail);
+    //   });
 
-        $message->subject('Smart Cookie Tutors.com - Cron Job Email');
-        $message->from('admin@SmartCookieTutors.com', 'Smart Cookie Tutors');
-        $message->to($toemail);
-      });
-
+    dd("asdf");
+      $sessions = DB::table('sessions')->where('status','Confirm')->get();
+      foreach ($sessions as $session) {
+        $session_date = $session->date;
+        $date1 =date("Y-m-d");
+        $date2 =$session->date;
+        if ($date1 == $date2) {
+          // date_default_timezone_set("America/New_York");
+          date_default_timezone_set("Asia/Karachi");
+          $time1 = date("h:i");
+          $time2 = date("h:i", strtotime('+2 hour +30 minutes',strtotime($session->time)));
+          // $time2 = date('h:i', strtotime($session->time));
+          // $new_time = date("h:i", strtotime('+2 hour +30 minutes'));
+          // $new_time2 = date("h:i", strtotime('+2 hour +30 minutes',strtotime($session->time)));
+          if ($time1 >= $time2) {
+            // dd($time1,$time2);
+            $input['status'] = 'End';
+            DB::table('sessions')->where('session_id',$session->session_id)->update($input);
+          }
+        }
+      }
 
 
     }
