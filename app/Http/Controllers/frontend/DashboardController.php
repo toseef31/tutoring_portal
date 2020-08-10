35e1e8678b1a374f93abf50d9b75bdfdac7fa2d2
peekinternational <peekinternational@gmail.com>
2020-08-10 13:02:45 +0500

    report

--- app/Http/Controllers/frontend/DashboardController.php
+++ app/Http/Controllers/frontend/DashboardController.php
@@ -371,7 +371,7 @@ class DashboardController extends Controller
          $tutors = DB::table('tutor_assign')
                   ->join('users','users.id','=','tutor_assign.tutor_id')
                   ->join('students','students.student_id','=','students.student_id')
-                  ->where('tutor_assign.user_id','=',auth()->user()->id)->orderby('users.first_name')->orderby('students.student_name')->paginate(10);
+                  ->where('tutor_assign.user_id','=',auth()->user()->id)->where('students.user_id','=',auth()->user()->id)->orderby('users.first_name')->orderby('students.student_name')->paginate(10);
                   // dd($tutors);
          $tutor_mobile = DB::table('tutor_assign')
                   ->join('users','users.id','=','tutor_assign.tutor_id')
