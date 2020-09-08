385bd3856ea6a6343ab9284d3ab38a9c7f38bac0
peekinternational <peekinternational@gmail.com>
2020-09-08 01:22:19 -0700

    session timess

--- resources/views/admin/view_session_details.blade.php
+++ resources/views/admin/view_session_details.blade.php
@@ -92,9 +92,51 @@
                          </td>
                         <td> {{$session->date}}</td>
                         <?php
-                        // $time = date('h:i a', strtotime($session->time))
-                        $tutor_timezone = Session::get('sct_admin')->time_zone;
-                        // dd($tutor_timezone);
+                        if ($session->added_by == 'Admin') {
+                          $tutor_timezone = $session->admin_timezone;
+                          $admin_timezone = Session::get('sct_admin')->time_zone;
+                          // Check session time zone and admin time zone
+                          if ($tutor_timezone == $admin_timezone) {
+                            $time = date('h:i a', strtotime($session->time));
+                            $date = date('M d, ', strtotime($session->date));
+                          }else {
+                              if ($tutor_timezone == 'Pacific Time') {
+                                date_default_timezone_set("America/Los_Angeles");
+                              }elseif ($tutor_timezone == 'Mountain Time') {
+                                date_default_timezone_set("America/Denver");
+                              }elseif ($tutor_timezone == 'Central Time') {
+                                date_default_timezone_set("America/Chicago");
+                              }elseif ($tutor_timezone == 'Eastern Time') {
+                                date_default_timezone_set("America/New_York");
+                              }
+                              $time1 = date('h:i a', strtotime($session->time));
+                              $date = date('M d, ', strtotime($session->date));
+                              $time_zone =SCT::getClientName($session->tutor_id)->time_zone;
+                              // dd($time_zone);
+                              $db_time = $session->date." ".$time1;
+                              $datetime = new DateTime($db_time);
+                              if ($time_zone == 'Pacific Time') {
+                                $la_time = new DateTimeZone('America/Los_Angeles');
+                                $datetime->setTimezone($la_time);
+                              }elseif ($time_zone == 'Mountain Time') {
+                                $la_time = new DateTimeZone('America/Denver');
+                                $datetime->setTimezone($la_time);
+                              }elseif ($time_zone == 'Central Time') {
+                                $la_time = new DateTimeZone('America/Chicago');
+                                $datetime->setTimezone($la_time);
+                              }elseif ($time_zone == 'Eastern Time') {
+                                $la_time = new DateTimeZone('America/New_York');
+                                $datetime->setTimezone($la_time);
+                              }
+                              $newdatetime = $datetime->format('Y-m-d h:i a');
+                              $get_datetime = explode(' ',$newdatetime);
+                              $time2 = $get_datetime[1];
+                              $time3 = $get_datetime[2];
+                              $time = $time2." ".$time3;
+                          }
+                        }else {
+                          // Added by Tutor
+                          $tutor_timezone = Session::get('sct_admin')->time_zone;
                         if ($tutor_timezone == 'Pacific Time') {
                           date_default_timezone_set("America/Los_Angeles");
                         }elseif ($tutor_timezone == 'Mountain Time') {
@@ -128,6 +170,8 @@
                         $time2 = $get_datetime[1];
                         $time3 = $get_datetime[2];
                         $time = $time2." ".$time3;
+                      }
+
                          ?>
                         <td>{{$time}}</td>
                         <td> {{$session->duration}}</td>
